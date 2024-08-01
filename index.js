aa8873731a3e39f85999dba0365ee3e004e1c723
Vasanthan100 <vasanthanraja23@gmail.com>
2024-08-01 09:17:55 +0530

    demo deployment to ec2

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import db from "./mongoC.js";
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.use((req, res, next) => {
+res.header('Access-Control-Allow-Origin','*');
+res.header('Access-Control-Allow-Headers', '*');
+next();
+});
+// Parses the text as url encoded data
+app.use(bodyParser.urlencoded ({extended: true}));
+// Parses the text as json
+app.use(bodyParser.json());
+app.get('/', async(req, res) => {
+res.send('Hello World, from express');
+})
+
+app.listen(PORT, () => {
+    console.log("Server running at port:",PORT);
+  });
--- mongoC.js
+++ mongoC.js
@@ -2,8 +2,9 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 // Call dotenv.config() to load environment variables from .env file
 dotenv.config();
-const password = encodeURIComponent (process.env.MONGO_PASSWORD.trim());
-//const fvdfv connectionString = 'mongodb+srv://integrationninjas:$(password)@dev-cluster.7nko6hh.mongodb.net/?retryWrites=true&w-majority&
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const connectionString = `mongodb+srv://vasanthanraja23:${password}@dev-cluster.b1v9yzk.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`;
+
 const client = new MongoClient (connectionString);
 let conn;
 try {
@@ -12,5 +13,5 @@ console.log("connection successful")
 } catch(e){
 console.error(e);
 }
-let db = conn.db("integration_ninjas");
+let db = conn.db("vasanthanraja_23");
 export default db;
